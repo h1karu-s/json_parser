9fff9971a832a8ab25e10f88c9674b3a9ceef2cc
hikaru-sh <polaris.2.71828@gmail.com>
2022-12-03 15:50:22 +0900

    change tox

--- json_parser.py
+++ json_parser.py
@@ -18,21 +18,5 @@ class JsonParser:
         if isinstance(self.data, list):
           return "list"
 
-    
     def get_json(self):
         return self.data
-
-            
-
-    # def parsing(self):
-    #     if isinstance(self.data, dict):
-    #         print('dict型です')
-    #         print(self.data.keys())
-    #     if isinstance(self.data, list):
-    #         print('list型です')
-    #         print(f"要素数は{len(self.data)}です")
-    #         d = self.data[0]
-    #         if isinstance(d, dict):
-    #             print(f"その下の階層にはdictになっており、keyは{d.keys()}")
-    #         if isinstance(d, list):
-    #             print(f"さらに下の階層もlistです。要素は{len(d)}です")
